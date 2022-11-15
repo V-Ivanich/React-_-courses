a4158529caa469411794a145d1490c42731f0d5a
V-Ivanich <chipai_dom@mail.ru>
2022-11-15 08:23:11 +0300

    again commit

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -59,25 +59,3 @@ const Counter = () => {
 }
 
 export default Counter
-
-//!---из другого задания пример
-// const renderUsers = () => {
-//   return users.map((user) => (
-//     <tr key={user._id}>
-//       <th scope="row">{user.name}</th>
-//       <td key={user._id}>
-//         {user.qualities.map((quality) => (
-//           <button className={`btn m-1 btn-` + quality.color}>
-//             {quality.name}
-//           </button>
-//         ))}
-//       </td>
-//       <td key={user.profession.id}>{user.profession.name}</td>
-//       <td>{user.completedMeetings}</td>
-//       <td>{user.rate}</td>
-//       <td>
-//         <button className="btn btn-danger">Delete</button>
-//       </td>
-//     </tr>
-//   ));
-// };
